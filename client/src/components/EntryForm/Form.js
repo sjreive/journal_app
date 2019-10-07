d866871bb7ef06bcda51aa20a3c188e9ad30c1cf
Sarah Reive <sarahjreive@gmail.com>
2019-10-07 00:04:07 -0400

    form styling

--- client/src/components/EntryForm/Form.js
+++ client/src/components/EntryForm/Form.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import classes from "./Form.module.scss";
 import { useVisualMode } from "../../hooks/useVisualMode";
-
+import { FaAngleDoubleLeft, FaAngleDoubleRight } from "react-icons/fa";
 import FeelingList from "./Feelings/FeelingList";
 import FeelingsTagList from "./Feelings/FeelingsTagList";
 
@@ -17,20 +17,14 @@ export default function Form(props) {
     }
 
     setEntryFeelings(prev => {
-      console.log("primary feeling:", primaryFeeling);
       return { ...prev, [feeling]: obj };
     });
     transition("SECOND");
   };
 
   const tagClickHandler = feeling => {
-    // add check to ensure key doesn't already exist.....
-    console.log(`Firing Click Handler.... with ${feeling}`);
-
     setEntryFeelings(prev => {
-      console.log("prev", prev);
       const copy = Object.keys(prev).filter(entryFeeling => {
-        console.log(entryFeeling);
         return entryFeeling !== feeling;
       });
       const copyPrev = {};
@@ -46,36 +40,89 @@ export default function Form(props) {
   };
 
   return (
-    <div className={classes.newEntryForm}>
-      {console.log("state:", entryFeelings)}
+    <div>
       {mode === "FIRST" && (
-        <div>
-          <h2>How do you feel right now?</h2>
+        <div className={classes.newEntryForm}>
+          <h1>How do you feel?</h1>
           <FeelingList
             feelings={props.feelings}
             clickHandler={feelingClickHandler}
+          />
+          <FeelingsTagList
+            feelings={entryFeelings}
+            clickHandler={tagClickHandler}
           />
         </div>
       )}
 
       {mode === "SECOND" && (
-        <div>
-          <div className={classes.primaryFeeling}>{primaryFeeling}</div>
-          <FeelingList
-            feelings={
-              entryFeelings &&
-              entryFeelings[Object.keys(entryFeelings)[0]] &&
-              entryFeelings[Object.keys(entryFeelings)[0]].subFeelings
-            }
-            clickHandler={feelingClickHandler}
+        <div className={classes.outerContainer}>
+          <div classNames={classes.backButton}>
+            <FaAngleDoubleLeft
+              className={classes.feelingTag__icon}
+              size={30}
+              onClick={() => transition("FIRST")}
+            />
+          </div>
+          <div className={classes.newEntryForm}>
+            <div className={classes.primaryFeeling}>
+              {primaryFeeling}{" "}
+              <FaAngleDoubleLeft
+                className={classes.feelingTag__icon}
+                size={30}
+                onClick={() => back()}
+              />{" "}
+              , or more specifically...
+            </div>
+            <FeelingList
+              feelings={
+                entryFeelings[primaryFeeling] &&
+                entryFeelings[primaryFeeling].subFeelings
+              }
+              clickHandler={feelingClickHandler}
+            />
+            <FeelingsTagList
+              feelings={entryFeelings}
+              clickHandler={tagClickHandler}
+            />
+          </div>
+          <FaAngleDoubleRight
+            className={classes.feelingTag__icon}
+            size={30}
+            onClick={() => transition("THIRD")}
           />
         </div>
       )}
-      <p>(mouseover a feeling for a description)</p>
-      <FeelingsTagList
-        feelings={entryFeelings}
-        clickHandler={tagClickHandler}
-      />
+
+      {mode === "THIRD" && (
+        <div className={classes.outerContainer}>
+          <div classNames={classes.backButton}>
+            <FaAngleDoubleLeft
+              className={classes.feelingTag__icon}
+              size={30}
+              onClick={() => transition("SECOND")}
+            />
+          </div>
+          <div className={classes.newEntryForm}>
+            <h2 className={classes.inputHeader}>
+              I feel
+              <FeelingsTagList
+                feelings={entryFeelings}
+                clickHandler={tagClickHandler}
+              />
+              because...
+            </h2>
+            <div className={classes.textInput}>
+              <input className={classes.input} type="text" />
+            </div>
+          </div>
+          <FaAngleDoubleRight
+            className={classes.feelingTag__icon}
+            size={30}
+            onClick={() => transition("THIRD")}
+          />
+        </div>
+      )}
     </div>
   );
 }
